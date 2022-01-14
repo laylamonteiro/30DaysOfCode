6a8ef62c88acb1d8bd62fa7f9e59319e8dd74c5a
laylindo <layla-sep@hotmai.com>
2022-01-13 22:03:07 -0300

    Add Task to Day1

--- src/Day1.java
+++ src/Day1.java
@@ -1,5 +1,15 @@
 import java.util.Scanner;
 
+//Task
+//Complete the code in the editor below. The variables i, d and s are already declared and initialized for you. You must:
+
+//Declare  variables: one of type int, one of type double, and one of type String.
+//Read  lines of input from stdin (according to the sequence given in the Input Format section below) and initialize your  variables.
+//Use the  operator to perform the following operations:
+//        Print the sum of  plus your int variable on a new line.
+//        Print the sum of  plus your double variable to a scale of one decimal place on a new line.
+//        Concatenate  with the string you read as input and print the result on a new line.
+
 public class Day1 {
 
     public static void main(String[] args) {
