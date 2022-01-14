fbc85da9853ae8d8bd69f25ea8c4bc48179b9350
laylindo <layla-sep@hotmai.com>
2022-01-13 22:00:01 -0300

    Add Task to Day3

--- src/Day3.java
+++ src/Day3.java
@@ -2,6 +2,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+//Task
+//Given an integer, , perform the following conditional actions:
+//
+//If  is odd, print Weird
+//If  is even and in the inclusive range of  to , print Not Weird
+//If  is even and in the inclusive range of  to , print Weird
+//If  is even and greater than , print Not Weird
+//Complete the stub code provided in your editor to print whether or not  is weird.
 
 public class Day3 {
     public static void main(String[] args) throws IOException {
