5129807a985aee30fa586e0255ece0ebe47340b0
laylindo <layla-sep@hotmai.com>
2022-01-13 22:01:50 -0300

    Add Task to Day2

--- src/Day2.java
+++ src/Day2.java
@@ -2,6 +2,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+//Task
+//Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip),
+//and tax percent (the percentage of the meal price being added as tax) for a meal, find and print the meal's
+//total cost. Round the result to the nearest integer.
+
+//Example
+//A tip of 15% * 100 = 15,
+//and the taxes are 8% * 100 = 8.
+//Print the value  and return from the function.
+
 class Result {
 
     /*
