626229f526e7077792fefc9b9bd953000b78b610
laylindo <layla-sep@hotmai.com>
2022-01-14 23:27:31 -0300

    Solve Day 4 - Day 4: Class vs. Instance

---
+++ src/Day4/Person.java
@@ -0,0 +1,54 @@
+package Day4;
+
+import java.util.Scanner;
+
+public class Person {
+    private int age;
+
+    public Person(int initialAge) {
+        // Add some more code to run some checks on initialAge
+        if (initialAge < 0) {
+            System.out.println("Age is not valid, setting age to 0.");
+            this.age = 0;
+        } else {
+            this.age = initialAge;
+        }
+    }
+
+    public void amIOld() {
+        // Write code determining if this person's age is old and print the correct statement:
+        String text = "";
+
+        if (this.age < 13) {
+            text = "You are young.";
+        } else if (this.age < 18) {
+            text = "You are a teenager.";
+        } else {
+            text = "You are old.";
+        }
+
+        System.out.println(text);
+    }
+
+    public void yearPasses() {
+        // Increment this person's age.
+        this.age = ++age;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int i = 0; i < T; i++) {
+            int age = sc.nextInt();
+            Person p = new Person(age);
+            p.amIOld();
+            for (int j = 0; j < 3; j++) {
+                p.yearPasses();
+            }
+            p.amIOld();
+            System.out.println();
+        }
+        sc.close();
+    }
+}
